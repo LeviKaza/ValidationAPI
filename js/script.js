b4d4bc6cab2ff48a149715b95cd13d403829e6fe
LeviKaza <admin@iMac-ITK-02.local>
2021-05-20 13:38:21 +0200

    information display

--- js/script.js
+++ js/script.js
@@ -4,19 +4,31 @@ fetch('https://api.artic.edu/api/v1/artworks')
 })
 .then((data) => {
     images = data.data;
+    var i = 1;
     images.forEach(image => {
         test = image.image_id;
         fetch('https://www.artic.edu/iiif/2/' + test + '/full/843,/0/default.jpg')
         .then((response) => {
             var div = document.createElement('figure');
             var p = document.createElement('figcaption');
-            p.setAttribute('class', 'none');
-            p.innerHTML = 'test';
             var img = document.createElement('img');
+            p.setAttribute('class', 'info' + i);
             img.setAttribute('src', response.url);
             div.append(img);
-            div.append(p)
+            div.append(p);
             document.querySelector('.pictures').append(div);
+            if(image.artist_title != null) {
+                document.querySelector('.info' + i).append('Auteur : ');
+                document.querySelector('.info' + i).append(image.artist_title);
+            }else {
+                document.querySelector('.info' + i).append('Auteur : ');
+                document.querySelector('.info' + i).append('Inconnu');
+            }
+            document.querySelector('.info' + i).append('\nTitre : ' + image.title);
+            document.querySelector('.info' + i).append('\nMatériaux : ' + image.medium_display);
+            document.querySelector('.info' + i).append('\nDate : ' + image.date_display);
+            document.querySelector('.info' + i).append('\nPeriode : ' + image.category_titles);
+            i++;
         })
     })
 })
