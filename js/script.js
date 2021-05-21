45b3a85d80b8a9532994b061ae1ae51df67278c0
LeviKaza <admin@iMac-ITK-02.local>
2021-05-21 11:32:18 +0200

    it works

--- js/script.js
+++ js/script.js
@@ -11,47 +11,63 @@ fetch('https://api.artic.edu/api/v1/artworks')
         i++;
     })
 })
+
+function search (i, el, url) {
+    fetch(url)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        if (data.data.length != 0) {
+            console.log(url);
+            url = data.pagination.next_url;
+            // debugger
+            search(i, el, url);
+        }
+    })
+}
+
 var i = 0;
 document.querySelector('.pictures').addEventListener("click", (el) => {
     el = el.target;
-    debugger
-    // if ()
-    console.log(i);
-    i++;
+    if (el.dataset.button) {
+        if (el.value != 'null') {
+            fetch('https://api.artic.edu/api/v1/artworks')
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                var url = data.pagination.next_url
+                images = data.data;
+                images.forEach(image => {
+                    if (image.artist_title === el.value) {
+                        test = image.image_id;
+                        fetch('https://www.artic.edu/iiif/2/' + test + '/full/843,/0/default.jpg')
+                        .then((response) => {
+                            var div = document.createElement('figure');
+                            var p = document.createElement('figcaption');
+                            var img = document.createElement('img');
+                            p.setAttribute('class', 'info' + i);
+                            img.setAttribute('src', response.url);
+                            div.append(img);
+                            div.append(p);
+                            document.querySelector('.pictures').append(div);
+                            if(image.artist_title != null) {
+                                document.querySelector('.info' + i).append('Auteur : ');
+                                document.querySelector('.info' + i).append(image.artist_title);
+                            }else {
+                                document.querySelector('.info' + i).append('Auteur : ');
+                                document.querySelector('.info' + i).append('Inconnu');
+                            }
+                            document.querySelector('.info' + i).append("\nTitre : " + image.title);
+                            document.querySelector('.info' + i).append("\nMatériaux : " + image.medium_display);
+                            document.querySelector('.info' + i).append("\nDate : " + image.date_display);
+                            document.querySelector('.info' + i).append("\nPeriode : " + image.category_titles);
+                        })
+                    search(i, el, url);
+                    }
+                })
+            })
+        }
+    }
 })
-// fetch('https://api.artic.edu/api/v1/artworks')
-// .then((response) => {
-//     return response.json();
-// })
-// .then((data) => {
-//     images = data.data;
-//     var i = 1;
-//     images.forEach(image => {
-//         if (image.artist_title === 'Giorgio de Chirico') {
-//             test = image.image_id;
-//             fetch('https://www.artic.edu/iiif/2/' + test + '/full/843,/0/default.jpg')
-//             .then((response) => {
-//                 var div = document.createElement('figure');
-//                 var p = document.createElement('figcaption');
-//                 var img = document.createElement('img');
-//                 p.setAttribute('class', 'info' + i);
-//                 img.setAttribute('src', response.url);
-//                 div.append(img);
-//                 div.append(p);
-//                 document.querySelector('.pictures').append(div);
-//                 if(image.artist_title != null) {
-//                     document.querySelector('.info' + i).append('Auteur : ');
-//                     document.querySelector('.info' + i).append(image.artist_title);
-//                 }else {
-//                     document.querySelector('.info' + i).append('Auteur : ');
-//                     document.querySelector('.info' + i).append('Inconnu');
-//                 }
-//                 document.querySelector('.info' + i).append("\nTitre : " + image.title);
-//                 document.querySelector('.info' + i).append("\nMatériaux : " + image.medium_display);
-//                 document.querySelector('.info' + i).append("\nDate : " + image.date_display);
-//                 document.querySelector('.info' + i).append("\nPeriode : " + image.category_titles);
-//                 i++;
-//             })
-//         }
-//     })
-// })
